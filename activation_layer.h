465f96294bd1877afa29bb23c71565d30bdc01df
Dharani_P <144601763+Srihanu1999@users.noreply.github.com>
2025-04-02 20:36:00 +0530

    Update activation_layer.h

--- activation_layer.h
+++ activation_layer.h
@@ -2,14 +2,8 @@
 #ifndef ACTIVATION_LAYER_H
 #define ACTIVATION_LAYER_H
 
-#include <cuda_runtime.h>
-
-__global__ void leaky_relu_kernel(float* input, int size, float alpha);
-__global__ void tanh_kernel(float* input, int size);
-__global__ void sigmoid_kernel(float* input, int size);
-
 void apply_leaky_relu(float* input, int size, float alpha);
 void apply_tanh(float* input, int size);
 void apply_sigmoid(float* input, int size);
 
-#endif
+#endif // ACTIVATION_LAYER_H
